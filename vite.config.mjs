c2a7ae98424d6ced4fa5239d93a524268670ae7f
Ramyak Coder <ramyak.coder@gmail.com>
2025-12-06 04:58:06 +0530

    updated vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".", // adjust if your source folder differs
+  base: "/pdf-annotater/",
   optimizeDeps: {
-    include: ['pdfjs-dist', 'tesseract.js']
+    include: ["pdfjs-dist", "tesseract.js"],
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('pdfjs-dist')) return 'pdfjs';
-          if (id.includes('tesseract.js')) return 'tesseract';
-        }
-      }
-    }
-  }
-})
+          if (id.includes("pdfjs-dist")) return "pdfjs";
+          if (id.includes("tesseract.js")) return "tesseract";
+        },
+      },
+    },
+  },
+});
